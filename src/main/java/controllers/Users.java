12c56b9508170a3ead4890f39e06725c72a4b9c5
94811 <94811@farnborough.ac.uk>
2020-12-03 09:09:42 +0000

    13/11

--- src/main/java/controllers/Users.java
+++ src/main/java/controllers/Users.java
@@ -57,5 +57,38 @@ public class Users {
             return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
         }
 
+        @POST
+        @Path("login")
+        public String UsersLogin(@FormDataParam("UserName") String UserName, @FormDataParam("PassWord") String PassWord) {
+            System.out.println("Invoked loginUser() on path users/login");
+            try {
+                PreparedStatement ps1 = Main.db.prepareStatement("SELECT PassWord FROM Users WHERE UserName = ?");
+                ps1.setString(1, UserName);
+                ResultSet loginResults = ps1.executeQuery();
+                if (loginResults.next() == true) {
+                    String correctPassword = loginResults.getString(1);
+                    if (PassWord.equals(correctPassword)) {
+                        String Token = UUID.randomUUID().toString();
+                        PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE UserName = ?");
+                        ps2.setString(1, Token);
+                        ps2.setString(2, UserName);
+                        ps2.executeUpdate();
+                        JSONObject userDetails = new JSONObject();
+                        userDetails.put("UserName", UserName);
+                        userDetails.put("Token", Token);
+                        return userDetails.toString();
+                    } else {
+                        return "{\"Error\": \"Incorrect password!\"}";
+                    }
+                } else {
+                    return "{\"Error\": \"Incorrect username.\"}";
+                }
+            } catch (Exception exception) {
+                System.out.println("Database error during /users/login: " + exception.getMessage());
+                return "{\"Error\": \"Server side error!\"}";
+            }
+        }
+
+
     }
 }
